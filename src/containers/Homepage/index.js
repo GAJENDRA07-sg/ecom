e791d585c6f55ea3bfb8e123bc5313b39562e7ba
GAJENDRA07-sg <gajendrasharma226@gmail.com>
2024-05-10 01:18:45 +0530

    changes done

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -8,9 +8,7 @@ import React from 'react';
 import Slider from './autoslider/auto';
 import Exam from './Carousel';
 import Section1 from './Section/Section1';
-import BlogPost from './Blog/BlogPost';
 import ChooseUs from './ChooseUs/ChooseUs';
-import OurClient from './OurClient/OurClient';
 import Testimonial from './Testimonial/Testimonial';
 import ProductCard from './Product/ProductCard';
 
@@ -22,11 +20,8 @@ function Homepage() {
     <div className="App">
       <Exam></Exam>
       <Section1></Section1>
-      {/* <ProductComponent></ProductComponent> */}
       <ProductCard></ProductCard>
       <Slider></Slider>
-      {/* <OurClient></OurClient> */}
-      {/* <BlogPost></BlogPost> */}
       <ChooseUs></ChooseUs>
       <Testimonial></Testimonial>
     </div>
